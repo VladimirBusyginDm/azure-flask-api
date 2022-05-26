905560502ddedefd1cac8a4674bf272955f138e3
Vladimir <vladimir.busygin@itrexgroup.com>
2022-05-26 16:07:44 +0300

    init commit

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
 
 application = Flask(__name__)
 
+answer = ['yes', 'no']
+
 @application.route('/')
 def hello_world():
-    return 'Hello world!'
+    return jsonify({'message' : 'Azure web api endpoint'})
 
-
+@application.route('/azure')
+def azure():
+    random.shuffle(answer)
+    return jsonify({'message' : random.choice(answer)})
 
 if __name__ == '__main__':
     application.run()
